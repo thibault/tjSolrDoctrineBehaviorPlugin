23e180e6a8418090da8ef3e2bd1415e45f905553
thibault <thibault@thibault-laptop.(none)>
2009-12-19 15:26:06 +0100

    up tests

--- test/unit/Doctrine/Template/Doctrine_Template_SolrTest.php
+++ test/unit/Doctrine/Template/Doctrine_Template_SolrTest.php
@@ -13,3 +13,22 @@ $t->ok(is_callable(array(Doctrine::getTable('Post'), 'isSearchAvailable')));
 // Ensure Solr is started, or will automaticaly fail
 $t->comment('-> test if Solr is reachable (will fail if not started)');
 $t->ok(Doctrine::getTable('Post')->isSearchAvailable());
+
+$t->comment('-> Search not existing content');
+$results = Doctrine::getTable('Post')->search('gloubigoulba');
+$t->is($results, array());
+
+$t->comment('-> Indexing content');
+
+$thread = new Thread();
+$thread->title = 'test thread';
+
+$post = new Post();
+$post->title = 'gloubigoulba';
+$post->body = 'this is my body';
+$post->Thread = $thread;
+$post->save();
+
+$results = Doctrine::getTable('Post')->search('gloubigoulba');
+$t->isnt($results, array());
+
