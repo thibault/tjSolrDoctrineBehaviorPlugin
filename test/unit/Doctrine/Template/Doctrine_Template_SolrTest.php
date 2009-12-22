83747bbf73a540117055071ad30838be3688db9a
thibault <thibault@thibault-laptop.(none)>
2009-12-22 13:39:33 +0100

    up test file

--- test/unit/Doctrine/Template/Doctrine_Template_SolrTest.php
+++ test/unit/Doctrine/Template/Doctrine_Template_SolrTest.php
@@ -14,21 +14,32 @@ $t->ok(is_callable(array(Doctrine::getTable('Post'), 'isSearchAvailable')));
 $t->comment('-> test if Solr is reachable (will fail if not started)');
 $t->ok(Doctrine::getTable('Post')->isSearchAvailable());
 
+// Ensure the index is empty
+$results = Doctrine::getTable('Post')->deleteIndex();
+
+// Should return no results
 $t->comment('-> Search not existing content');
 $results = Doctrine::getTable('Post')->search('gloubigoulba');
-$t->is($results, array());
+$t->is($results->numFound, 0);
 
 $t->comment('-> Indexing content');
-
-$thread = new Thread();
-$thread->title = 'test thread';
-
 $post = new Post();
 $post->title = 'gloubigoulba';
 $post->body = 'this is my body';
-$post->Thread = $thread;
+$post->Thread = new Thread();
+$post->Thread->title = 'test thread';
 $post->save();
 
-$results = Doctrine::getTable('Post')->search('gloubigoulba');
-$t->isnt($results, array());
+$otherPost = new Post();
+$otherPost->title = 'foobar';
+$otherPost->body = 'This is a gloubigoulba body';
+$otherPost->Thread = $post->Thread;
+$otherPost->save();
 
+$results = Doctrine::getTable('Post')->search('gloubigoulba');
+$t->is($results->numFound, 2);
+
+$t->comment('-> Deleting content');
+$post->delete();
+$results = Doctrine::getTable('Post')->search('gloubigoulba');
+$t->is($results->numFound, 1);
