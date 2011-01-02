65671e366c83e21b8854ef8ec2ed16c45b45cc1f
Thibault Jouannic <teeboo@gmail.com>
2011-01-02 18:04:51 +0100

    add query parameters to createSearchQueryTableProxy

--- lib/Doctrine/Template/Solr.php
+++ lib/Doctrine/Template/Solr.php
@@ -140,9 +140,9 @@ class Doctrine_Template_Solr extends Doctrine_Template
    *
    * @return Doctrine_Query
    **/
-  public function createSearchQueryTableProxy($search, $limit = 256)
+  public function createSearchQueryTableProxy($search, $offset=0, $limit = 30, array $params = array())
   {
-    $response = $this->getTable()->search($search, 0, $limit);
+    $response = $this->getTable()->search($search, $offset, $limit, $params);
 
     $pks = array();
     foreach($response['response']['docs'] as $doc)
